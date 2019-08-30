819ed4c9257d0b11b25e56788dd1d9bf8d9eec37
Magdalena Pawłowska <pawlowskam123@gmail.com>
2019-08-30 14:17:31 +0200

    Zadanie5

---
+++ Pulpit/PierwszyProgram/Main5.java
@@ -0,0 +1,13 @@
+
+public class Main5 {
+
+	public static void main(String[] args) {
+		System.out.print("Dzisiaj zaczynam kurs");
+		System.out.print("języka JAVA");
+		System.out.print("w CodersLab");
+
+	}
+
+}
+
+//klasa nie zgadza się z nazwą pliku
