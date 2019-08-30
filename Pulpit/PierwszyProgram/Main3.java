5599b9180090e6c8a2f2e8e0549914fe964f52b3
Magdalena Pawłowska <pawlowskam123@gmail.com>
2019-08-30 14:16:07 +0200

    Zadanie3

---
+++ Pulpit/PierwszyProgram/Main3.java
@@ -0,0 +1,19 @@
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		System.out.println("Pierwszy program JAVA");
+
+	}
+
+}
+
+//komentarz jednolinijkowy
+
+/*
+komentarz
+wielolinijkowy
+ */
+
+/** komentarz dokumentacyjny */
+
